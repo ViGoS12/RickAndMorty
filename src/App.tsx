4450405e713fe3fdd754230af0f156ac1a452530
Kirill <kirisakov4@yandex.ru>
2022-07-31 14:03:43 +0300

    Create start settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import './scss/App.scss'
+import Logo from './assets/svg/logo.svg'
 
 function App() {
-  return <div className='App'>Hello</div>
+  return (
+    <div className='App'>
+      <div className='App__container'>
+        <img className='logo' src={Logo} alt='' />
+      </div>
+    </div>
+  )
 }
 
 export default App
