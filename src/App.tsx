4c17a867c4150f79584444da1d3449595730d765
Kirill <kirisakov4@yandex.ru>
2022-08-01 10:32:28 +0300

    Added basic axioms request

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,43 @@
-import './scss/App.scss'
-import Logo from './assets/svg/logo.svg'
+import Header from './components/header'
+import axios from 'axios'
+
+import './scss/app.scss'
+import { useEffect, useState } from 'react'
+
+type items = Record<string, string | number>
 
 function App() {
+  const [items, setItems] = useState<Array<items>>([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  const fetchItems = async () => {
+    setIsLoading(true)
+    try {
+      const { data } = await axios.get(
+        `https://rickandmortyapi.com/api/character`
+      )
+      const { results = [] } = data
+      setItems(results)
+    } catch (error) {
+      console.log('error', error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  console.log(items)
+
+  useEffect(() => {
+    fetchItems()
+  }, [])
+
   return (
-    <div className='App'>
-      <div className='App__container'>
-        <img className='logo' src={Logo} alt='' />
+    <div className='app'>
+      <div className='app__container'>
+        <Header />
+        {items.map((obj) => {
+          return <div key={obj.id}>{obj.name}</div>
+        })}
       </div>
     </div>
   )
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,19 @@
+import styles from './Header.module.scss'
+import Logo from '../../assets/svg/logo.svg'
+
+const Header: React.FC = () => {
+  return (
+    <div className={styles.header}>
+      <img className={styles.header__logo} src={Logo} alt='' />
+      <ul className={styles.header__list}>
+        <li className={styles.header__item}>feature 1</li>
+        <li className={styles.header__item}>feature 2</li>
+        <li className={styles.header__item}>feature 3</li>
+        <li className={styles.header__item}>feature 4</li>
+        <li className={styles.header__item}>feature 5</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Header
